12a1d214788c2900152efb98f9bbd1f62289037a
william pereira <williampereira21@gmail.com>
2022-06-20 01:35:41 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'linkedin-clone-mobo',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
