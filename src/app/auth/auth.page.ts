ca41f662a73ce2e22a2eaea761b0f09500f171a2
william pereira <williampereira21@gmail.com>
2022-06-24 20:27:07 -0300

    implementado tela de login

---
+++ src/app/auth/auth.page.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-auth',
+  templateUrl: './auth.page.html',
+  styleUrls: ['./auth.page.scss'],
+})
+export class AuthPage implements OnInit {
+  @ViewChild('form') form: NgForm;
+
+  submissionType: 'login' | 'join' = 'login';
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  onSubmit() {
+
+  }
+
+  toggleText() {
+    if (this.submissionType === 'login') {
+      this.submissionType = 'join';
+    } else if (this.submissionType === 'join') {
+      this.submissionType = 'login';
+    }
+  }
+
+}
